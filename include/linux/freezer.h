f833bc8438bd3a3abbc97b5edafbdf2732bb6b69
Dennis Boyce <dsb9938@yahoo.com>
2013-02-06 22:29:36 -0500

    Linux 3.4.25
    Patch from: http://www.kernel.org/pub/linux/kernel/v3.x/incr/

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -50,9 +50,7 @@ static inline bool cgroup_freezing(struct task_struct *task)
 {
 	return false;
 }
-#endif 
-
-
+#endif
 
 static inline void freezer_do_not_count(void)
 {
@@ -62,12 +60,26 @@ static inline void freezer_do_not_count(void)
 static inline void freezer_count(void)
 {
 	current->flags &= ~PF_FREEZER_SKIP;
+	/*
+	 * If freezing is in progress, the following paired with smp_mb()
+	 * in freezer_should_skip() ensures that either we see %true
+	 * freezing() or freezer_should_skip() sees !PF_FREEZER_SKIP.
+	 */
+	smp_mb();
 	try_to_freeze();
 }
 
-static inline int freezer_should_skip(struct task_struct *p)
+static inline bool freezer_should_skip(struct task_struct *p)
 {
-	return !!(p->flags & PF_FREEZER_SKIP);
+	/*
+	 * The following smp_mb() paired with the one in freezer_count()
+	 * ensures that either freezer_count() sees %true freezing() or we
+	 * see cleared %PF_FREEZER_SKIP and return %false.  This makes it
+	 * impossible for a task to slip frozen state testing after
+	 * clearing %PF_FREEZER_SKIP.
+	 */
+	smp_mb();
+	return p->flags & PF_FREEZER_SKIP;
 }
 
 
