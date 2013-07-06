8b8b8e30e96ade913ab0d41162211e641067a860
James Taylor <jmztaylor@gmail.com>
2013-07-06 16:44:32 -0400

    Added Force Fast Charge

--- arch/arm/mach-msm/htc_battery_8960.c
+++ arch/arm/mach-msm/htc_battery_8960.c
@@ -40,6 +40,9 @@
 #include <mach/htc_charger.h>
 #include <mach/htc_battery_cell.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
 
 #define HTC_BATT_CHG_DIS_BIT_EOC	(1)
 #define HTC_BATT_CHG_DIS_BIT_ID		(1<<1)
@@ -600,19 +603,33 @@ static void cable_status_notifier_func(enum usb_connect_type online)
 
 	switch (online) {
 	case CONNECT_TYPE_USB:
+#ifdef CONFIG_FORCE_FAST_CHARGE
+    /* If forced fast charge is enabled "always" or if no USB device detected, go AC */
+    if ((force_fast_charge == FAST_CHARGE_FORCE_AC) ||
+        (force_fast_charge == FAST_CHARGE_FORCE_AC_IF_NO_USB &&
+                      USB_peripheral_detected == USB_ACC_NOT_DETECTED        )) {
+      BATT_LOG("cable USB forced to AC");
+      is_fast_charge_forced = FAST_CHARGE_FORCED;
+      current_charge_mode = CURRENT_CHARGE_MODE_AC;
+      htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_AC);
+    } else {
+      BATT_LOG("cable USB not forced to AC");
+      is_fast_charge_forced = FAST_CHARGE_NOT_FORCED;
+      current_charge_mode = CURRENT_CHARGE_MODE_USB;
+      htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_USB);
+    }
+#else
 		BATT_LOG("USB charger");
 		htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_USB);
-		
 		break;
+#endif
 	case CONNECT_TYPE_AC:
 		BATT_LOG("5V AC charger");
 		htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_AC);
-		
 		break;
 	case CONNECT_TYPE_WIRELESS:
 		BATT_LOG("wireless charger");
 		htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_WIRELESS);
-		
 		break;
 	case CONNECT_TYPE_UNKNOWN:
 		BATT_LOG("unknown type");
@@ -620,7 +637,6 @@ static void cable_status_notifier_func(enum usb_connect_type online)
 		break;
 	case CONNECT_TYPE_INTERNAL:
 		BATT_LOG("delivers power to VBUS from battery (not supported)");
-		
 		htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_INTERNAL);
 		break;
 	case CONNECT_TYPE_CLEAR:
@@ -631,7 +647,6 @@ static void cable_status_notifier_func(enum usb_connect_type online)
 	case CONNECT_TYPE_NONE:
 		BATT_LOG("No cable exists");
 		htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_NONE);
-		
 		break;
 	case CONNECT_TYPE_MHL_AC:
 		BATT_LOG("mhl_ac");
@@ -640,7 +655,6 @@ static void cable_status_notifier_func(enum usb_connect_type online)
 	default:
 		BATT_LOG("unsupported connect_type=%d", online);
 		htc_charger_event_notify(HTC_CHARGER_EVENT_SRC_NONE);
-		
 		break;
 	}
 #if 0 
