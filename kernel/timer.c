6fae876b350a86607fa6fbb827448367931c50de
James Taylor <jmztaylor@gmail.com>
2013-07-08 13:20:24 -0400

    Fix last commit

--- kernel/timer.c
+++ kernel/timer.c
@@ -1160,6 +1160,8 @@ static int __cpuinit init_timers_cpu(int cpu)
 		base = per_cpu(tvec_bases, cpu);
 	}
 
+	spin_lock_irqsave(&base->lock, flags);
+
 	for (j = 0; j < TVN_SIZE; j++) {
 		INIT_LIST_HEAD(base->tv5.vec + j);
 		INIT_LIST_HEAD(base->tv4.vec + j);
